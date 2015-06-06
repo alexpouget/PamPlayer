4f1d706535c9d3a0ae50b211848e85d9b765454a
Baba D <pacers_7@hotmail.fr>
2015-06-06 13:22:45 +0200

    little amendment

--- src/Graphique/MyMouse.java
+++ src/Graphique/MyMouse.java
@@ -6,7 +6,7 @@ import java.awt.event.MouseEvent;
 public class MyMouse extends MouseAdapter {
 		public void mouseClicked(MouseEvent evt) {
 	       // if (evt.getClickCount() == 2) {
-	        	System.out.println("nice catch up kid");
+	        	//System.out.println("nice catch up kid");
 	            // Double-click detected
 	       // } else if (evt.getClickCount() == 3) {   
 	        }
