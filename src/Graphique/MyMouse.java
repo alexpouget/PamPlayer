11891b10fb9dc6ff83fa1d454dfe9c03038ea3f4
Loki <bmichau@LFR004458.corp.capgemini.com>
2015-06-12 18:13:07 +0200

    Implementation de la fonction qui recupère et traite le flux rss de news

--- src/Graphique/MyMouse.java
+++ src/Graphique/MyMouse.java
@@ -2,11 +2,29 @@ package graphique;
 
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.io.IOException;
+
+import javax.swing.JList;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.jdom2.JDOMException;
+import org.xml.sax.SAXException;
+
+import newsgeneration.NewsGenerator;
 
 public class MyMouse extends MouseAdapter {
 		public void mouseClicked(MouseEvent evt) {
 	        if (evt.getClickCount() == 2) {
-	        	System.out.println("nice catch up kid");
+	        	JList list=(JList)evt.getSource();
+	        	String row= list.getSelectedValue().toString();
+	        	try {
+					NewsGenerator.Redirect(row);
+	        		
+				} catch (ParserConfigurationException | SAXException
+						| IOException | JDOMException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
 	            // Double-click detected
 	        } else if (evt.getClickCount() == 3) {
 	        }
--- src/Music/Album.java
+++ src/Music/Album.java
@@ -21,4 +21,9 @@ public class Album {
     public void setName(String name) {
         this.name = name;
     }
+    
+    public String toString()
+    {
+    	return this.name;
+    }
 }
