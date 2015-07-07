a4f32fd0479a0a5b394504aeff7359a5df547690
Loki <bmichau@LFR004458.corp.capgemini.com>
2015-07-07 22:18:37 +0200

    creation classe user instanciation a la connexion

--- src/Graphique/evenement/MyConnect.java
+++ src/Graphique/evenement/MyConnect.java
@@ -5,17 +5,21 @@ import graphique.MyWindow;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class MyConnect implements ActionListener {
+import users.User;
 
+public class MyConnect implements ActionListener {
+	static User currentUser;
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		
 		String mdpEnString= new String(MyConnexion.fieldMdp.getPassword());
+		
 		if(database.VerifConnexion.verifConnexion(MyConnexion.fieldLogin.getText(), mdpEnString)==true)
 		{
 			MyConnexion.labelErreurCo.setVisible(false);
 			MyConnexion.connexion.dispose();
 			MyWindow.labelConnected.setText("Vous est connecté en tant que "+MyConnexion.fieldLogin.getText());
+			currentUser= new User(MyConnexion.fieldLogin.getText(), mdpEnString);
 		}
 		else
 		{
