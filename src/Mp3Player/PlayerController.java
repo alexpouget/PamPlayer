8a3bb7a75b669596f491f1fcc963cc39b895e5e2
alexpouget <pouget.al@gmail.com>
2015-07-19 15:08:09 +0200

    clean and comment p2

--- src/Mp3Player/PlayerController.java
+++ src/Mp3Player/PlayerController.java
@@ -10,6 +10,9 @@ import java.io.FileNotFoundException;
 
 /**
  * Created by alex on 06/06/2015.
+ *
+ * class qui crée un objet player et implementes les methodes pour lancer et gerer ce player dans
+ * un thread a part c'est pour cela qu'il implements la class runnable
  */
 public class PlayerController implements Runnable {
     private SongPlayer player;
@@ -19,15 +22,18 @@ public class PlayerController implements Runnable {
     private String music;
     private PlayerListener listener;
 
+    //constructeur
     public PlayerController(String fileName) {
         music = fileName;
     }
 
     @Override
     public void run() {
+        //on set le status actif pour indiquer une lecture en cours
         status = Status.ACTIVE;
         try {
             if (player != null)
+                //on fait appel a la method play de songplayer
                 player.play(position, Integer.MAX_VALUE);
         } catch (JavaLayerException e) {
             e.printStackTrace();
@@ -42,6 +48,7 @@ public class PlayerController implements Runnable {
         play();
     }
 
+    //pour la pause on stop le player et on stock l'emplacement ou on s'est arreté
     public void pause() {
         if (player == null)
             return;
@@ -51,6 +58,7 @@ public class PlayerController implements Runnable {
         player = null;
     }
 
+    //pour lancer la musique on crée un objet songplayer et un thread
     public void play() {
         try {
             player = new SongPlayer(new FileInputStream(music), music);
@@ -64,9 +72,8 @@ public class PlayerController implements Runnable {
         t.start();
     }
 
-
+    //pour stopper on detruit le lecteur et on remet la position a 0
     public void stop() {
-
         if (player == null)
             return;
         status = Status.FINISHED;
@@ -75,7 +82,6 @@ public class PlayerController implements Runnable {
         player.close();
         player = null;
         MyWindow.infoMusic.setText("");
-
     }
 
     public String getMusic() {
@@ -88,9 +94,8 @@ public class PlayerController implements Runnable {
         return player.getFrameNumber();
     }
 
-
+    //method pour l'avance rapide
     public void avanceTo(int i) {
-
         position = (i - position);
     }
 
--- src/Mp3Player/Status.java
+++ src/Mp3Player/Status.java
@@ -2,6 +2,8 @@ package mp3Player;
 
 /**
  * Created by alex on 06/06/2015.
+ *
+ * Enumeration des status possible du player
  */
 public enum Status {
     ACTIVE,
