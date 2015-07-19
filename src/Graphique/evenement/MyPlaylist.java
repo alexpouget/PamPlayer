4858e697e24a9446e045a5900be2bb4c7498d9b1
Baba D <pacers_7@hotmail.fr>
2015-07-19 21:53:15 +0200

    refresh JTREE when song is deleted

--- src/Graphique/evenement/MyPlaylist.java
+++ src/Graphique/evenement/MyPlaylist.java
@@ -35,157 +35,136 @@ import music.Music;
 // Listerner qui permet la gestion des actions du clique droit la liste des musiques
 public class MyPlaylist extends MouseAdapter{
 	public void mouseClicked(MouseEvent e) {
-		
+
 	}
 
 	@Override
 	public void mouseEntered(MouseEvent e) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void mouseExited(MouseEvent e) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void mousePressed(MouseEvent e) {
 		// Left mouse click
-			
-				// Right mouse click
-				if ( SwingUtilities.isRightMouseButton( e ))
-				{
-					// get the coordinates of the mouse click
-					Point p = e.getPoint();
-		 
-					// get the row index that contains that coordinate
-					int rowNumber = MyWindow.tableau.rowAtPoint( p );
-		 
-					// Get the ListSelectionModel of the JTable
-					ListSelectionModel model = MyWindow.tableau.getSelectionModel();
-		 
-					// set the selected interval of rows. Using the "rowNumber"
-					// variable for the beginning and end selects only that one row.
-					model.setSelectionInterval( rowNumber, rowNumber );
-					JPopupMenu menuPopUpClickDroit= new JPopupMenu();
-					MyWindow.tableau.setComponentPopupMenu(menuPopUpClickDroit);
-					JMenuItem itemMenuClickDroit=new JMenuItem("Ajouter a la playlist");
-					menuPopUpClickDroit.add(itemMenuClickDroit);
-					JMenuItem itemMenuClickDroit2=new JMenuItem("Supprimer la chanson");
-					menuPopUpClickDroit.add(itemMenuClickDroit2);
-					menuPopUpClickDroit.show(e.getComponent(), e.getX(), e.getY());
-					
-					// evenement lorsque l'on clique sur ajouter a la playlist
-					itemMenuClickDroit.addActionListener(new ActionListener() {
-						
-			
-						
-						@Override
-						// ajout de la musique Ã  la playlist
-						public void actionPerformed(ActionEvent e) {
-							
-							
-					            int row = MyWindow.tableau.getSelectedRow();
-					            Object musicAAjouter = MyWindow.tableau.getValueAt(row, 0);
-					            Music music = null;
-	                            for(Music m : MyWindow.listMusic)
-	                            {
-	                                if(m.getTitle().equals(musicAAjouter.toString()))
-	                                    music = m;
-	                            }
 
-	                         
-					            MyWindow.persoTablePlaylist.addMusic(music);
-					          
-						}
-					});
-					
-					// suppression de la musique de la bibliotheque
-					itemMenuClickDroit2.addActionListener(new ActionListener() {
-						
-						public void actionPerformed(ActionEvent e) {
-                            int row = MyWindow.tableau.getSelectedRow();
-                            Object cellule = MyWindow.tableau.getValueAt(row, 0);
-                            Music music = null;
-                            for(Music m : MyWindow.listMusic)
-                            {
-                                if(m.getTitle().equals(cellule.toString()))
-                                    music = m;
-                            }
-                            
-                            ListMusic listMusic = new ListMusic();
-                            listMusic.removeMusic(music);
-                            MyWindow.persoTableModel.removeMusic(music);
-                            
-                            	if(MyWindow.listMusic.contains(music)){
-                            			MyWindow.listMusic.remove(music);
-                            	}
-                            
-                            DefaultTreeModel model = (DefaultTreeModel)MyWindow.arbre.getModel();
-                            
-                            DefaultMutableTreeNode root = (DefaultMutableTreeNode)model.getRoot();
-                            String lartiste = music.getArtiste().getName().toUpperCase().trim();
-                            String lalbum = music.getAlbum().getName().trim();
+		// Right mouse click
+		if ( SwingUtilities.isRightMouseButton( e ))
+		{
+			// get the coordinates of the mouse click
+			Point p = e.getPoint();
 
-                            DefaultMutableTreeNode artiste = new DefaultMutableTreeNode(lartiste);
-                            
-                            DefaultMutableTreeNode album = new DefaultMutableTreeNode(lalbum);
-                            
-                            boolean artistExist = false;
-                            boolean multipleAlbum = false;
-                            
-                            for (int i = 0; i < root.getChildCount(); i++){
-                            		if(root.getChildAt(i).toString().equals(lartiste)){
-                            			if(root.getChildAt(i).getChildCount()==1)
-                            				root.remove(i);
-                            			artistExist = true;
-                            			System.out.println("egal, GetChildCount: "+root.getChildAt(i).getChildCount());
-                            		}
+			// get the row index that contains that coordinate
+			int rowNumber = MyWindow.tableau.rowAtPoint( p );
 
-                        			int j=0, k=0, albumNodeInt=0;
-                            		if(root.getChildAt(i).getChildCount()>1){
-                            			for( k=0; k < root.getChildAt(i).getChildCount(); k++){
-                                			if(root.getChildAt(i).getChildAt(k).toString().equals(lalbum)){
-                                				System.out.println("lalbum:"+root.getChildAt(i).getChildAt(k).toString()+", k:"+k);
-                                				albumNodeInt=k;
-                                			}
-                            			}
-                            			for(Music son : MyWindow.listMusic){
-//                        					System.out.println("son: "+son);
-                        					if(son.getAlbum().getName().equals(lalbum)){
-                        						j++;
-                        						System.out.println("j: "+j);
-//                        						if(j>1)
-//                        							break;
-                        				}
-                        			}
+			// Get the ListSelectionModel of the JTable
+			ListSelectionModel model = MyWindow.tableau.getSelectionModel();
 
-                            			if(j==1){
-                            				//DefaultMutableTreeNode artNode = new DefaultMutableTreeNode(root.getChildAt(i));				
-                            				//DefaultMutableTreeNode albumNode = new DefaultMutableTreeNode(root.getChildAt(i).getChildAt(albumNodeInt));
-//                            				artNode.remove(albumNode);
-                            				model.removeNodeFromParent(root.getChildAt(i));
-//                            				root.add(artNode);
-//                            				artNode.add(albumNode);
-                            				//System.out.println("artNode: "+artNode+", artnodecount: "+artNode.getChildCount()+", getchildcount:"+root.getChildAt(i).getChildCount());
-                            				//artNode.remove(albumNodeInt);
-//                            				model.removeNodeFromParent((MutableTreeNode) root.getChildAt(i).getChildAt(albumNodeInt));
-//                            				
-                            				//System.out.println("albumNode: "+albumNode);
-                        				}
-                            }
-						}
-                            
-                            MyWindow.arbre.updateUI();
-                    		model.reload(root);
+			// set the selected interval of rows. Using the "rowNumber"
+			// variable for the beginning and end selects only that one row.
+			model.setSelectionInterval( rowNumber, rowNumber );
+			JPopupMenu menuPopUpClickDroit= new JPopupMenu();
+			MyWindow.tableau.setComponentPopupMenu(menuPopUpClickDroit);
+			JMenuItem itemMenuClickDroit=new JMenuItem("Ajouter a la playlist");
+			menuPopUpClickDroit.add(itemMenuClickDroit);
+			JMenuItem itemMenuClickDroit2=new JMenuItem("Supprimer la chanson");
+			menuPopUpClickDroit.add(itemMenuClickDroit2);
+			menuPopUpClickDroit.show(e.getComponent(), e.getX(), e.getY());
+
+			// evenement lorsque l'on clique sur ajouter a la playlist
+			itemMenuClickDroit.addActionListener(new ActionListener() {
+
+
+
+				@Override
+				// ajout de la musique Ã  la playlist
+				public void actionPerformed(ActionEvent e) {
+
+
+					int row = MyWindow.tableau.getSelectedRow();
+					Object musicAAjouter = MyWindow.tableau.getValueAt(row, 0);
+					Music music = null;
+					for(Music m : MyWindow.listMusic)
+					{
+						if(m.getTitle().equals(musicAAjouter.toString()))
+							music = m;
 					}
-				});
-		
-	}
 
 
+					MyWindow.persoTablePlaylist.addMusic(music);
 
+				}
+			});
+
+			// suppression de la musique de la bibliotheque
+			itemMenuClickDroit2.addActionListener(new ActionListener() {
+
+				public void actionPerformed(ActionEvent e) {
+					int row = MyWindow.tableau.getSelectedRow();
+					Object cellule = MyWindow.tableau.getValueAt(row, 0);
+					Music music = null;
+					for(Music m : MyWindow.listMusic)
+					{
+						if(m.getTitle().equals(cellule.toString()))
+							music = m;
+					}
+
+					ListMusic listMusic = new ListMusic();
+					listMusic.removeMusic(music);
+					MyWindow.persoTableModel.removeMusic(music);
+					
+					DefaultTreeModel model = (DefaultTreeModel)MyWindow.arbre.getModel();
+
+					DefaultMutableTreeNode root = (DefaultMutableTreeNode)model.getRoot();
+					String lartiste = music.getArtiste().getName().toUpperCase().trim();
+					String lalbum = music.getAlbum().getName().trim();
+					int nbrMusicAyantLalbum=0;
+					
+					/*parcourt de la listmusic qui implémente le JTree et incrémentation de la vazriable
+					 nbrMusicAyantLalbum afin de savoir si l'album de la chanson suppprimée contient plusieurs
+					 chansons */
+					
+					try{
+					for(Music son : MyWindow.listMusic){
+						if(son.getAlbum().getName().equals(lalbum)){
+							nbrMusicAyantLalbum++;
+						}
+					}
+					
+					/*Si l'album ne contient qu'une seul chanson alors on parcourt les noeuds de l'arbre
+					  de facon a s'arreter sur le noeud concernant lartiste de la chanson supprimée. Si 
+					  lartiste contient plusieurs albums alors on ne supprime que le noeud de l'album concerné.
+					  Si l'artiste ne contient qu'un seul album alors on supprime le noeud de lartiste.*/
+					
+							if(nbrMusicAyantLalbum==1){
+								for (int i = 0; i <= root.getChildCount(); i++){
+									if(root.getChildAt(i).toString().equals(lartiste)){
+										for(int k=0; k < root.getChildAt(i).getChildCount(); k++){
+											if(root.getChildAt(i).getChildCount()==1){
+												model.removeNodeFromParent((MutableTreeNode) root.getChildAt(i));
+											}
+											else{
+												if(root.getChildAt(i).getChildAt(k).toString().equals(lalbum))
+													model.removeNodeFromParent((MutableTreeNode) root.getChildAt(i).getChildAt(k));
+												}
+										}
+									}
+								}
+							}
+							//Suppression de la chanson de la listArtist et mise à jour du JTree							
+						MyWindow.listMusic.remove(music);
+					MyWindow.arbre.updateUI();
+					model.reload(root);
+				}catch(Exception ev){}
+				}
+			});
+
+		}
 	}
 }
